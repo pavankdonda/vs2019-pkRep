e9d4bc2eb822ed2959ad2bc53a095f8bb1585bae
Pavan <dondapavankumar@gmail.com>
2021-11-13 12:22:27 +0530

    Add project files.

---
+++ vs2019-pkRep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs2019_pkRep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
